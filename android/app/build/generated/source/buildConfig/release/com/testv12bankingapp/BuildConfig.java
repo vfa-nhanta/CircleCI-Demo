2919e42b719e708386d498f664445d33361a111e
Nhan Tran <vfa.nhanta@gmail.com>
2018-11-28 14:56:35 +0700

    v12 android signing

---
+++ android/app/build/generated/source/buildConfig/release/com/testv12bankingapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.testv12bankingapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.testv12bankingapp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
